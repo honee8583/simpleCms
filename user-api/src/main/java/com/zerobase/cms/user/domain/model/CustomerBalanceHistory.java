923415b3dc9dffaf0244cd57c6a85dfbf8714ade
leedahoon <honee8583@naver.com>
2022-11-25 16:19:26 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현
    ChangeBalanceForm의 changeMoney 필드에 사용요금을 입력할시 '-'를 붙여야 함.

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/CustomerBalanceHistory.java
@@ -0,0 +1,33 @@
+package com.zerobase.cms.user.domain.model;
+
+
+import lombok.*;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@ToString
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class)
+public class CustomerBalanceHistory extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
+    private Customer customer;
+
+    private Integer changeMoney;    // 변경된 돈 량
+
+    private Integer currentMoney;   // 해당 시점 잔액
+
+    private String fromMessage;
+
+    private String description;
+}
+
